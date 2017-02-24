f4e6950f8047e832087b752fa2ee62543d18e47f
Vkiro <vkiro@yandex.ua>
2017-02-24 23:01:41 +0200

    Add tasks

---
+++ src/week2/task3/ArraySort.java
@@ -0,0 +1,22 @@
+package week2.task3;
+
+/**
+ * Напишіть застосування для сортування масиву методом бульбашки.
+ */
+public class ArraySort {
+
+    public static void main(String[] args) {
+        int[] array = {30, 2, 10, 4, 6};
+        int length = array.length;
+
+        //PUT YOUR CODE HERE
+
+
+        //PUT YOUR CODE HERE
+
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
+
---
+++ src/week2/task4/ShellSort.java
@@ -0,0 +1,20 @@
+package week2.task4;
+
+/**
+ * Напишіть застосування для сортування масиву методом сортування Шелла (ShellSort). Завдання додаткове!
+ */
+public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] array = {30, 2, 10, 4, 6};
+        int length = array.length;
+
+        //PUT YOUR CODE HERE
+
+        //PUT YOUR CODE HERE
+
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
