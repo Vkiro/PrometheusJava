7064eef3fa8d9cc10ef22c74fbbbde61c5898f4e
Vkiro <vkiro@yandex.ua>
2017-02-24 22:37:59 +0200

    Initial commit

---
+++ src/week2/task5/BinarySearch.java
@@ -0,0 +1,22 @@
+package week2.task5;
+
+/**
+ * Напишіть застосування, що виконує пошук заданого числа у відсортованому масиві — бінарний пошук
+ *
+ * У випадку коли число знайдено виведіть на екран його позицію в масиві (позиції нумеруємо з нуля) або -1 в іншому випадку
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        int data[] = { 3, 6, 7, 10, 34, 56, 60 };
+        int numberToFind = 10;
+
+        // PUT YOUR CODE HERE
+        for (int i = data.length / 2; i > 0;) {
+            if (data[i] > numberToFind) {
+
+            }
+        }
+        // PUT YOUR CODE HERE
+    }
+}
