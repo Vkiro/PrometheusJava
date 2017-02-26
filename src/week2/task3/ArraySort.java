9fe34e96816a804048ef7e429b64909ba5063959
Vkiro <vkiro@yandex.ua>
2017-02-26 12:36:53 +0200

    Initial commit

--- src/week2/task3/ArraySort.java
+++ src/week2/task3/ArraySort.java
@@ -13,9 +13,9 @@ public class ArraySort {
         for (int i = 0; i < length - 1; i++) {
             for (int j = 0; j < length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
-                    int accum = array[j];
+                    int tmp = array[j];
                     array[j] = array[j + 1];
-                    array[j + 1] = accum;
+                    array[j + 1] = tmp;
                 }
             }
         }
